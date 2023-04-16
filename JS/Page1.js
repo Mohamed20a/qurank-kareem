92eb47a12d820623e94a48c585d5dfe74890e8b9
Mohamed Ahmed <100856846+Mohamed20a@users.noreply.github.com>
2023-04-16 15:24:16 +0200

    Add files via upload

--- JS/Page1.js
+++ JS/Page1.js
@@ -1,15 +1,23 @@
-let menu = document.querySelector('.navbar-toggler');
-let navbar = document.querySelector('.navbar .collapse');
-let header = document.querySelector('.navbar');
+// Select Items
+const menu = document.querySelector('.navbar-toggler');
+const navbar = document.querySelector('.navbar .collapse');
+const header = document.querySelector('.navbar');
+const fixedNav = document.querySelector(".navbar");
+const scrollBtn = document.querySelector(".scrollBtn");
+const exploreBtn = document.querySelector('.title .btn1');
+const praySection = document.querySelector('.pray');
+const cards = document.querySelector('.cards');
 
+// Add click event on menu button
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
 }
 
+// Add scroll event
 window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+menu.classList.remove('fa-times');
+navbar.classList.remove('active');
 
 if(window.scrollY > 0){
     header.classList.add('active');
@@ -17,91 +25,61 @@ if(window.scrollY > 0){
     header.classList.remove('active');
 }
 
+scrollup();
 }
 
-
-
-let fixedNav = document.querySelector(".navbar"),
-scrollBtn = document.querySelector(".scrollBtn");
-window.addEventListener("scroll", ()=>{
-scrollup()
-})
+// Add click event for scroll up button
 scrollBtn.addEventListener('click',()=>{
-window.scrollTo({
-top : 0 ,
-behavior : "smooth"
-})
-})
-scrollup()
-function scrollup(){
-window.scrollY > 50 ? fixedNav.classList.add('active') : fixedNav.classList.
-remove('active');
-window.scrollY > 100 ? scrollBtn.classList.add('active') : scrollBtn.classList.
-remove('active'); 
-}
-
-
-
-let exploreBtn = document.querySelector('.title .btn'),
-    praySection = document.querySelector('.pray');
-exploreBtn.addEventListener('click',()=>{
-    praySection.scrollIntoView({
-        behavior : "smooth"
+    window.scrollTo({
+    top : 0 ,
+    behavior : "smooth"
     })
-})   
-
-
-
-
-
-let cards = document.querySelector('.cards');
-getPrayTime();
-function getPrayTime()
-{
-
-
+    })
+    
+    // Add click event on explore button
+    exploreBtn.addEventListener('click',()=>{
+    praySection.scrollIntoView({
+    behavior : "smooth"
+    })
+    })
+    
+    // Call the prayer function
+    getPrayTime();
+    
+    // Prayer Function
+    function getPrayTime()
+    {
     fetch("https://api.aladhan.com/v1/timingsByCity?city=cairo&country=egypt&method=8")
     .then(response => response.json())
     .then(data => {
-        
+
         let times = data.data.timings;
-        
+    
+        // Calculating time according to the 12-hour system
         const prayersTimes = {
-            الفجر: times.Imsak,
-            الشروق: times.Sunrise,
-            الظهر: times.Dhuhr,
-            العصر: times.Asr,
-            المغرب: times.Sunset,
-            العشاء: times.Isha,
+            الفجر: new Date(`01/01/2000 ${times.Imsak}`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+            الشروق: new Date(`01/01/2000 ${times.Sunrise}`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+            الظهر: new Date(`01/01/2000 ${times.Dhuhr}`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+            العصر: new Date(`01/01/2000 ${times.Asr}`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+            المغرب: new Date(`01/01/2000 ${times.Sunset}`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+            العشاء: new Date(`01/01/2000 ${times.Isha}`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
         };
-
-        
-
-        
+    
         cards.innerHTML = '';
         
-            for (let time in prayersTimes)
-            {
-                
-                
-                cards.innerHTML+= 
-                
-                
-                `
-                    <div class="card">
-                        <div class="circle">
-                            <svg>
-                                <Circle cx="100" cy="100" r="100"></Circle>
-                            </svg>
-                            <div class="prayTime">${prayersTimes[time]}</div>
-                        </div>
-                        <p>${time}</p>
-                        
+        for (let time in prayersTimes)
+        {
+            cards.innerHTML+= `
+                <div class="card">
+                    <div class="circle">
+                        <svg>
+                            <Circle cx="100" cy="100" r="100"></Circle>
+                        </svg>
+                        <div class="prayTime">${prayersTimes[time]}</div>
                     </div>
-                `
-                }
-            
-        
+                    <p>${time}</p>
+                </div>
+            `;
+        }
     })
-    
-}  
+}    
